592d7f17f7b2b1b2687e561fc2e492f279584a5d
Gregor <jakob.srumpf@gmail.com>
2022-02-09 00:04:08 +0100

    added icon, frame, having trouble chainging the size of titlebar

--- main.py
+++ main.py
@@ -8,11 +8,48 @@ class App(tkinter.Tk):
         super().__init__()
         self.miliseconds = tkinter.StringVar(value="0")
 
-        self.resizable(False,False)
-        self.geometry("443x335")# don t change
-        self.frame1 = Frame(self, border=5, bg="green", height=100, width=100)
-        self.frame1.pack()
+        self.overrideredirect(True)
+        self.attributes('-topmost', 1)
+        self.iconbitmap('./assets/click_icon.ico')
+        self.title("Auto Clicker 1.0")
 
+        self.resizable(False,False)
+        self.geometry('443x335')# don t change +0+0
+
+        self.title_bar = Frame(self, bg='white', bd=1)
+        close_button = Button(self.title_bar, text='X', command=self.destroy)
+        window = Canvas(self, bg='#f0f0f0')
+
+        # pack the widgets
+        self.title_bar.pack(side=TOP, fill=X)
+        close_button.pack(side=RIGHT)
+        window.pack(expand=1, fill=BOTH)
+
+        #bind title bar motion to the move window function
+        self.title_bar.bind('<B1-Motion>', self.get_pos)
+        
+        self.frame1 = LabelFrame(window, height=44, text="Click interval")
+        self.frame1.pack(fill=X, padx=2, pady=2)
+
+        #LabelFrame(self, text="This is a LabelFrame").pack(fill=X)
+        #labelframe.pack(fill="both", expand="yes")
+
+    def get_pos(self, event):
+        xwin = self.winfo_x()
+        ywin = self.winfo_y()
+        startx = event.x_root
+        starty = event.y_root
+
+        ywin = ywin - starty
+        xwin = xwin - startx
+
+
+        def move_window(event):
+            self.geometry("443x335" + '+{0}+{1}'.format(event.x_root + xwin, event.y_root + ywin))
+        startx = event.x_root
+        starty = event.y_root
+
+        self.title_bar.bind('<B1-Motion>', move_window)
 
 
 if __name__ == '__main__':
