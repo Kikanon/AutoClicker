017375aa54e31546a5ce634dec05e3e33daa808c
Gregor <jakob.srumpf@gmail.com>
2022-06-13 16:55:03 +0200

    added main window elements

--- autoclicker.py
+++ autoclicker.py
@@ -1,10 +1,17 @@
 from cgitb import grey
+from msilib.schema import RadioButton
 import tkinter
 from tkinter import *
 import tkinter.font
-from turtle import back, bgcolor
+from turtle import back, bgcolor, width
+import os
 
 from matplotlib.pyplot import fill
+
+class TaskBar(Tk):    
+    def __init__(self):
+        Tk.__init__(self)
+        self.attributes('-alpha', 0.0)
 
 class App(tkinter.Tk):
     window_size = '442x365'
@@ -13,41 +20,69 @@ class App(tkinter.Tk):
         super().__init__()
         self.miliseconds = tkinter.StringVar(value="0")
 
+        self.iconbitmap(default=os.path.abspath("./assets/click_icon.ico"))
+
         self.attributes('-topmost', 1)
         self.title("Auto Clicker 1.0")
 
         self.resizable(False,False)
         self.geometry(self.window_size)# don t change +0+0
 
-        # todo: https://stackoverflow.com/questions/4066027/making-tkinter-windows-show-up-in-the-taskbar
-
+        self.repeatSelection=IntVar()
+        self.mouseButtonSelection=StringVar()
+        self.mouseButtonSelection.set("Left")
+        self.mouseClickSelection=StringVar()
+        self.mouseClickSelection.set("Single")
         gray_area = Canvas(self, bg='#f0f0f0')
 
         # pack the widgets
         gray_area.pack(expand=1, fill=BOTH)
             
-        
-        self.frame1 = LabelFrame(gray_area, height=44, text="Click interval")
-        self.frame1.pack(fill=X, padx=2, pady=2)
+        frame1 = LabelFrame(gray_area, height=44, text="Click interval")
+        frame1.pack(fill=X, padx=2, pady=2)
 
-        Entry(self.frame1, width=7).pack(side=LEFT)
-        Label(self.frame1, text="hours").pack(side=LEFT)
-        Entry(self.frame1, width=7).pack(side=LEFT)
-        Label(self.frame1, text="mins").pack(side=LEFT)
-        Entry(self.frame1, width=7).pack(side=LEFT)
-        Label(self.frame1, text="secs").pack(side=LEFT)
-        Entry(self.frame1, width=7).pack(side=LEFT)
-        Label(self.frame1, text="miliseconds").pack(side=LEFT)
+        Entry(frame1, width=7).pack(side=LEFT)
+        Label(frame1, text="hours").pack(side=LEFT)
+        Entry(frame1, width=7).pack(side=LEFT)
+        Label(frame1, text="mins").pack(side=LEFT)
+        Entry(frame1, width=7).pack(side=LEFT)
+        Label(frame1, text="secs").pack(side=LEFT)
+        Entry(frame1, width=7).pack(side=LEFT)
+        Label(frame1, text="miliseconds").pack(side=LEFT)
 
-        self.frame2 = LabelFrame(gray_area, height=44, text="Click options")
+        optionsFrame = Frame(gray_area, height=45)
+        frame2 = LabelFrame(optionsFrame, text="Click options")
+        frame3 = LabelFrame(optionsFrame, text="Click repeat")
+        positionFrame = LabelFrame(gray_area, height=50, text="Cursor position", borderwidth=2, relief="groove")
+        buttonsFrame = Frame(gray_area,height=50, borderwidth=2, relief="groove")
 
-        self.frame3 = LabelFrame(gray_area, height=44, text="Click repeat")
+        optionsFrame.pack(fill=X)
+        positionFrame.pack(fill=X)
+        buttonsFrame.pack(fill=X)
+        frame2.pack(padx=2, pady=2, expand=True, fill=BOTH, side=LEFT)
+        frame3.pack(padx=2, pady=2, expand=True, fill=BOTH, side=LEFT)
 
-        self.frame2.pack(side=LEFT, padx=2, pady=2, fill=X)
-        self.frame3.pack(side=LEFT, padx=2, pady=2, fill=X)
+        options1 = Frame(frame2)
+        options2 = Frame(frame2)
+        options1.pack(side=TOP)
+        options2.pack(side=TOP)
 
-        Entry(self.frame2, width=7).pack(side=LEFT)
-        Label(self.frame2, text="hours").pack(side=LEFT)
+        Label(options1, text="Mouse button: ").pack(side=LEFT)
+        OptionMenu(options1, self.mouseButtonSelection, "Left", "Right", "Middle").pack(side=LEFT)
+
+        Label(options2, text="Click type: ").pack(side=LEFT)
+        OptionMenu(options2, self.mouseClickSelection, "Single", "Double").pack(side=LEFT)
+
+        repeatOptions1 = Frame(frame3)
+        repeatOptions2 = Frame(frame3)
+        repeatOptions1.pack(side=TOP)
+        repeatOptions2.pack(side=TOP)
+
+        Radiobutton(repeatOptions1, text="Repeat", variable=self.repeatSelection, value=1).pack(side=LEFT)
+        Radiobutton(repeatOptions2, text="Repeat until stopped", variable=self.repeatSelection, value=2).pack()
+
+        Entry(repeatOptions1, width=7).pack(side=LEFT)
+        Label(repeatOptions1, text="times").pack(side=LEFT)
 
         #LabelFrame(self, text="This is a LabelFrame").pack(fill=X)
         #labelframe.pack(fill="both", expand="yes")
